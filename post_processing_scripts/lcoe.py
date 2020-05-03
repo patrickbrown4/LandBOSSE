728657f09a5a4ceddfb2de82f5311c47ea834343
Alicia Key <akey7@users.noreply.github.com>
2020-05-03 17:24:26 -0600

    Issue #142
    Write a .csv file at the end and print logging messages along the way.

--- post_processing_scripts/lcoe.py
+++ post_processing_scripts/lcoe.py
@@ -2,12 +2,14 @@ import pandas as pd
 
 if __name__ == '__main__':
     # Select every row from the AEP and TCC files
+    print('Reading AEP and TCC data...')
     aep = pd.read_csv('aep.csv')
     tcc = pd.read_csv('tcc.csv')
 
     # Select every row from the LandBOSSE output.
     # Only select the needed columns, convert MW to kW and rename the columns
     # to be consistent with the AEP and TCC data.
+    print('Reading BOS data...')
     bos = pd.read_csv('extended_landbosse_costs.csv')
     bos = bos[['Number of turbines', 'Turbine rating MW', 'Hub height m', 'Labor cost multiplier', 'Crane breakdown fraction', 'Rotor diameter m', 'Cost per project']]
     bos['Rating [kW]'] = bos['Turbine rating MW'] * 1000
@@ -15,17 +17,27 @@ if __name__ == '__main__':
     bos.drop(columns=['Turbine rating MW'], inplace=True)
 
     # Aggregate and sum BOS costs
+    print('Aggregating BOS costs...')
     bos_sum = bos.groupby(['Rating [kW]', 'Rotor Diam [m]', 'Number of turbines', 'Hub height m', 'Labor cost multiplier', 'Crane breakdown fraction']).sum().reset_index()
 
     # Inner join AEP and TCC. Taken together, Rating [kW] and Rotor Diam [m]
     # are the key.
+    print('Joining AEP and TCC...')
     aep_tcc = aep.merge(tcc, on=['Rating [kW]', 'Rotor Diam [m]'])
+
+    if len(aep_tcc) == 0:
+        raise Exception('aep_tcc merge is empty')
 
     # Then join in the BOS sum data, again using Rating [kW] and
     # Rotor Diam [m] as keys. This dataframe will eventually have the
     # LCOE as a column.
+    print('Joining aep_tcc and bos_sum...')
     lcoe = aep_tcc.merge(bos_sum, on=['Rating [kW]', 'Rotor Diam [m]'])
 
+    if len(bos_sum) == 0:
+        raise Exception('bos_sum is empty.')
+
+    print('Calculatig the LCOE...')
     # Create columns for FCR and Opex USD/kW
     lcoe['FCR'] = 0.079
     lcoe['Opex [USD/kW]'] = 52.0
@@ -35,11 +47,9 @@ if __name__ == '__main__':
     lcoe['Turbine Capex [USD]'] = lcoe['TCC [USD/kW]'] * lcoe['Rating [kW]'] * lcoe['Number of turbines']
     capex_times_fcr = (lcoe['BOS Capex [USD]'] + lcoe['Turbine Capex [USD]']) * lcoe['FCR']
     aep_all_turbines = lcoe['AEP [kWh/yr]'] * lcoe['Number of turbines']
-    lcoe['LCOE [USD/kW]'] = (capex_times_fcr + lcoe['Total Opex [USD]']) / aep_all_turbines
+    lcoe['LCOE [USD/kWh]'] = (capex_times_fcr + lcoe['Total Opex [USD]']) / aep_all_turbines
 
-    # Output an Excel spreadsheet with the results.
-    with pd.ExcelWriter('lcoe_analysis.xlsx', mode='w') as writer:
-        lcoe.to_excel(writer, sheet_name='LCOE', index=False)
-        bos.to_excel(writer, sheet_name='BOS', index=False)
-        aep.to_excel(writer, sheet_name='AEP', index=False)
-        tcc.to_excel(writer, sheet_name='TCC', index=False)
+    print(f'Writing LCOE analysis with {len(lcoe)} rows...')
+    lcoe.to_csv('lcoe_analysis.csv', index=False)
+
+    print('Done writing LCOE analysis.')
